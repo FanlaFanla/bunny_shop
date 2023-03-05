372245faa103f5ba911de3cf1698341415661df6
FanlaFanla <cloudkku@163.com>
2023-03-05 09:26:58 +0800

    项目初始化

--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,19 @@
 import { createStore } from 'vuex'
 
 export default createStore({
+  // 存状态
   state: {
   },
+  // 计算包装
   getters: {
   },
+  // 更新状态
   mutations: {
   },
+  // 异步更新状态
   actions: {
   },
+  // 模块
   modules: {
   }
 })
